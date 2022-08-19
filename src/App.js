d2ff2790a30d0e34e8fd730f4665908e90429eba
Ashutosh Kumar <53196209+TiwariAshuism@users.noreply.github.com>
2022-08-19 23:13:40 +0530

    Tiwari Foundation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Component/Navbar';
+import Header from './Component/Hero';
+export const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div>
+        <Header/>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App;
